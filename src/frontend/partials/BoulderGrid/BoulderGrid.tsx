f15e2b05855b64908a1ad6c210a11a7ee9b630cd
BelexB <s-azubei@haw-landshut.de>
2021-12-09 16:41:33 +0100

    styling: moved input-field

--- src/frontend/partials/BoulderGrid/BoulderGrid.tsx
+++ src/frontend/partials/BoulderGrid/BoulderGrid.tsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { ChangeEvent, useState } from "react";
 import { orderBy } from "lodash";
 
 import { BoulderHeaderRow } from "../BoulderHeaderRow/BoulderHeaderRow";
 import { Boulder } from "../../../models/common";
 import { BoulderRow } from "../BoulderRow/BoulderRow";
 import { tableHeader } from "../../constants/labels";
+import { Input } from "../../components/Input/Input";
+import { retrieveBoulders } from "../../helper/requests";
 
 interface BoulderGridProps {
   boulderData?: Boulder[];
@@ -23,6 +25,28 @@ export const BoulderGrid = ({
 }: BoulderGridProps) => {
   const [sortedData, setSortedData] = useState([]);
   const [sortingKey, setSortingKey] = useState(null);
+  const [searchedArea, setSearchedArea] = useState(null);
+
+  const handleAreaChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchedArea(event.target.value);
+  };
+
+  const handleAreaSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!searchedArea) {
+      alert("Please add an area");
+
+      return;
+    }
+    retrieveBoulders(searchedArea)
+      .then((boulder) => {
+        setSearchedBoulders(boulder);
+      })
+      .catch((err) => {
+        console.error(err.message);
+      });
+  };
 
   const handleSort = (key: string) => {
     if (sortingKey !== key) {
@@ -43,9 +67,15 @@ export const BoulderGrid = ({
       <BoulderHeaderRow
         labels={tableHeader}
         onSort={handleSort}
-        setSearchedBoulders={setSearchedBoulders}
         setFetchedBoulders={setFetchedBoulders}
       />
+      <form className="boulder-grid__search-bar" onSubmit={handleAreaSubmit}>
+        <Input
+          name="searchArea"
+          placeholder="Area"
+          onChange={handleAreaChange}
+        />
+      </form>
       <div className="boulder-grid__rows">
         {boulderData?.map((boulder, index) => (
           <BoulderRow
--- src/frontend/partials/BoulderHeaderRow/BoulderHeaderRow.tsx
+++ src/frontend/partials/BoulderHeaderRow/BoulderHeaderRow.tsx
@@ -9,73 +9,36 @@ import { Input } from "../../components/Input/Input";
 interface BoulderHeaderRowProps {
   labels: object;
   onSort?: (label: string) => void;
-  setSearchedBoulders: (boulder: Boulder[]) => void;
   setFetchedBoulders: (boulder: Boulder[]) => void;
 }
 
 export const BoulderHeaderRow = ({
   labels,
   onSort,
-  setSearchedBoulders,
   setFetchedBoulders,
 }: BoulderHeaderRowProps) => {
-  const [searchedArea, setSearchedArea] = useState(null);
-
-  const handleAreaChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setSearchedArea(event.target.value);
-  };
-
-  const handleAreaSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    if (!searchedArea) {
-      alert("Please add an area");
-
-      return;
-    }
-    retrieveBoulders(searchedArea)
-      .then((boulder) => {
-        setSearchedBoulders(boulder);
-      })
-      .catch((err) => {
-        console.error(err.message);
-      });
-  };
-
   const handleButtonClick = () => {
     retrieveUserBoulders().then((boulders) => setFetchedBoulders(boulders));
   };
 
   return (
-    <div
-      className={buildClassName(
-        "boulder-header-row",
-        { header: true },
-        "boulder-header-row__cells"
-      )}
-    >
-      <form className="area-form" onSubmit={handleAreaSubmit}>
-        <Input
-          name="searchArea"
-          placeholder="Area"
-          onChange={handleAreaChange}
-        />
-      </form>
-
-      {Object.values(labels).map((label) => {
-        return (
-          <div className="boulder-header-row__cell">
-            <Button key={label} label={label} onClick={() => onSort(label)} />
-          </div>
-        );
-      })}
-      <div className="boulder-header-row__button">
-        <Button
-          color="red"
-          label={window.location.pathname == "/" ? "User Table" : "Go Back"}
-          linkTo={window.location.pathname == "/" ? "/userTable" : "/"}
-          onClick={window.location.pathname == "/" ? handleButtonClick : null}
-        />
+    <div className={buildClassName("boulder-header-row", { header: true })}>
+      <div className="boulder-header-row__cells">
+        {Object.values(labels).map((label) => {
+          return (
+            <div className="boulder-header-row__cell">
+              <Button key={label} label={label} onClick={() => onSort(label)} />
+            </div>
+          );
+        })}
+        <div className="boulder-header-row__button">
+          <Button
+            color="red"
+            label={window.location.pathname == "/" ? "User Table" : "Go Back"}
+            linkTo={window.location.pathname == "/" ? "/userTable" : "/"}
+            onClick={window.location.pathname == "/" ? handleButtonClick : null}
+          />
+        </div>
       </div>
     </div>
   );
